4a063312bf233a0f4c60d687db390426041c4131
Cesare <cesarec88@gmail.com>
2017-08-23 09:54:48 +0200

    add one description

--- pre_processing/brain_pipeline.py
+++ pre_processing/brain_pipeline.py
@@ -8,6 +8,13 @@ import numpy as np
 import subprocess
 import progressbar
 
+__author__ = "Cesare Catavitello"
+__license__ = "MIT"
+__version__ = "1.0.1"
+__maintainer__ = "Cesare Catavitello"
+__email__ = "cesarec88@gmail.com"
+__status__ = "Production"
+
 # np.random.seed(5)  # for reproducibility
 progress = progressbar.ProgressBar(widgets=[progressbar.Bar('*', '[', ']'), progressbar.Percentage(), ' '])
 
